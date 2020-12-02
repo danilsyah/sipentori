d142f558e4f2361bfc31719a6f1694ebfea40fb0
danilsyah <danilsyaharihardjo@gmail.com>
2020-12-02 15:50:23 +0700

    menambahkan ukuran schema migration pada AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
         });
         config(['app.locate' => 'id']);
         date_default_timezone_set('Asia/Jakarta');
+	Schema::defaultStringLength(191);
     }
 
     public function register()
